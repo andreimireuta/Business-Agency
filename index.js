c9dd45d4380199c3ad1c6e739f304ef3adf924fd
andreimireuta <andreymyreuta@gmail.com>
2021-12-14 13:11:49 +0200

    Added menu script

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // prompt("Please tell me a number");
+
+const menu = document.querySelector('.menu');
+const menu1 = document.querySelector('.menu1');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', ()=>{
+    navbar.classList.toggle('change');
+    menu.classList.toggle('visible-hidden');
+    menu1.classList.toggle('visible-active');
+})
+menu1.addEventListener('click', ()=>{
+    navbar.classList.toggle('change');
+    menu.classList.toggle('visible-hidden');
+    menu1.classList.toggle('visible-active');
+})
